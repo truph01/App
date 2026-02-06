6cda2d00a37975e9f1483cfbdd7b1faa42896aac
Wojciech Boman <wojciechboman@gmail.com>
2026-02-06 09:04:50 +0100

    Fix navigating back from WalletPage

--- src/pages/workspace/WorkspaceMembersPage.tsx
+++ src/pages/workspace/WorkspaceMembersPage.tsx
@@ -554,6 +554,11 @@ function WorkspaceMembersPage({personalDetails, route, policy}: WorkspaceMembers
     );
 
     useEffect(() => {
+        if (isMobileSelectionModeEnabled) {
+            return;
+        }
+
+        setSelectedEmployees([]);        
         return () => {
             if (!isMobileSelectionModeEnabled) {
                 return;
