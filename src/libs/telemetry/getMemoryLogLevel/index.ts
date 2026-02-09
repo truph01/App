9f0953d45527e886e76ef97a4850d9f218853aa2
Carlos Martins <cmartins@expensify.com>
2026-02-09 23:37:54 +0100

    Fix prettier

--- src/libs/telemetry/getMemoryLogLevel/index.ts
+++ src/libs/telemetry/getMemoryLogLevel/index.ts
@@ -1,6 +1,6 @@
 import type * as Sentry from '@sentry/react-native';
-import CONST from '@src/CONST';
 import type {MemoryInfo} from '@libs/telemetry/getMemoryInfo/types';
+import CONST from '@src/CONST';
 
 /**
  * Determine memory log level for web platform based on JS heap percentage
