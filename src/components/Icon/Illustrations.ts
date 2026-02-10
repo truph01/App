bc216c54a65c78c049011fef5123d5e7fca1cf94
Georgia Monahan <georgia@expensify.com>
2026-02-10 14:02:02 +0000

    add more empty states

--- src/components/Icon/Illustrations.ts
+++ src/components/Icon/Illustrations.ts
@@ -13,24 +13,30 @@ import Alert from '@assets/images/simple-illustrations/simple-illustration__aler
 import Approval from '@assets/images/simple-illustrations/simple-illustration__approval.svg';
 import Binoculars from '@assets/images/simple-illustrations/simple-illustration__binoculars.svg';
 import BlueShield from '@assets/images/simple-illustrations/simple-illustration__blueshield.svg';
+import Broom from '@assets/images/simple-illustrations/simple-illustration__broom.svg';
 import Buildings from '@assets/images/simple-illustrations/simple-illustration__buildings.svg';
 import CalendarMonthly from '@assets/images/simple-illustrations/simple-illustration__calendar-monthly.svg';
 import CarIce from '@assets/images/simple-illustrations/simple-illustration__car-ice.svg';
 import Car from '@assets/images/simple-illustrations/simple-illustration__car.svg';
+import Chair from '@assets/images/simple-illustrations/simple-illustration__chair.svg';
 import ChatBubbles from '@assets/images/simple-illustrations/simple-illustration__chatbubbles.svg';
+import CheckboxText from '@assets/images/simple-illustrations/simple-illustration__checkboxtext.svg';
 import CheckmarkCircle from '@assets/images/simple-illustrations/simple-illustration__checkmarkcircle.svg';
 import Clock from '@assets/images/simple-illustrations/simple-illustration__clock.svg';
 import CommentBubbles from '@assets/images/simple-illustrations/simple-illustration__commentbubbles.svg';
 import ConciergeBot from '@assets/images/simple-illustrations/simple-illustration__concierge-bot.svg';
 import ConciergeBubble from '@assets/images/simple-illustrations/simple-illustration__concierge-bubble.svg';
+import CowboyHat from '@assets/images/simple-illustrations/simple-illustration__cowboy-hat.svg';
 import CreditCardEyes from '@assets/images/simple-illustrations/simple-illustration__creditcardeyes.svg';
 import CreditCardsNewGreen from '@assets/images/simple-illustrations/simple-illustration__creditcards--green.svg';
 import EmailAddress from '@assets/images/simple-illustrations/simple-illustration__email-address.svg';
 import EmptyShelves from '@assets/images/simple-illustrations/simple-illustration__empty-shelves.svg';
 import Encryption from '@assets/images/simple-illustrations/simple-illustration__encryption.svg';
 import EnvelopeReceipt from '@assets/images/simple-illustrations/simple-illustration__envelopereceipt.svg';
+import F1Flags from '@assets/images/simple-illustrations/simple-illustration__f1-flags.svg';
 import FastMoney from '@assets/images/simple-illustrations/simple-illustration__fastmoney.svg';
 import Filters from '@assets/images/simple-illustrations/simple-illustration__filters.svg';
+import FishbowlBlue from '@assets/images/simple-illustrations/simple-illustration__fishbowl--blue.svg';
 import Flash from '@assets/images/simple-illustrations/simple-illustration__flash.svg';
 import Gears from '@assets/images/simple-illustrations/simple-illustration__gears.svg';
 import HeadSet from '@assets/images/simple-illustrations/simple-illustration__headset.svg';
@@ -44,7 +50,12 @@ import Luggage from '@assets/images/simple-illustrations/simple-illustration__lu
 import MagnifyingGlassReceipt from '@assets/images/simple-illustrations/simple-illustration__magnifyingglass-receipt.svg';
 import Mailbox from '@assets/images/simple-illustrations/simple-illustration__mailbox.svg';
 import Pencil from '@assets/images/simple-illustrations/simple-illustration__pencil.svg';
+import SmallRocket from '@assets/images/simple-illustrations/simple-illustration__smallrocket.svg';
+import Sunglasses from '@assets/images/simple-illustrations/simple-illustration__sunglasses.svg';
+import Target from '@assets/images/simple-illustrations/simple-illustration__target.svg';
 import ThumbsUpStars from '@assets/images/simple-illustrations/simple-illustration__thumbsupstars.svg';
+import Trophy1 from '@assets/images/simple-illustrations/simple-illustration__trophy-1.svg';
+import Trophy from '@assets/images/simple-illustrations/simple-illustration__trophy.svg';
 import ExpensifyApprovedLogo from '@assets/images/subscription-details__approvedlogo.svg';
 import TurtleInShell from '@assets/images/turtle-in-shell.svg';
 
@@ -54,17 +65,21 @@ export {
     Approval,
     Binoculars,
     BlueShield,
+    Broom,
     Buildings,
     CalendarMonthly,
     Car,
     CarIce,
+    Chair,
     ChatBubbles,
+    CheckboxText,
     CheckmarkCircle,
     Clock,
     CommentBubbles,
     Computer,
     ConciergeBot,
     ConciergeBubble,
+    CowboyHat,
     CreditCardEyes,
     CreditCardsNewGreen,
     EmailAddress,
@@ -74,10 +89,12 @@ export {
     Encryption,
     EnvelopeReceipt,
     ExpensifyApprovedLogo,
+    F1Flags,
     ExpensifyCardImage,
     FastMoney,
     Filters,
     Fireworks,
+    FishbowlBlue,
     Flash,
     Gears,
     HeadSet,
@@ -96,6 +113,11 @@ export {
     Pencil,
     PendingTravel,
     Puzzle,
+    SmallRocket,
+    Sunglasses,
+    Target,
     ThumbsUpStars,
+    Trophy,
+    Trophy1,
     TurtleInShell,
 };
--- src/pages/home/ForYouSection/EmptyState.tsx
+++ src/pages/home/ForYouSection/EmptyState.tsx
@@ -1,60 +1,151 @@
 import React from 'react';
 import {View} from 'react-native';
+import type {IllustrationName} from '@components/Icon/chunks/illustrations.chunk';
 import ImageSVG from '@components/ImageSVG';
 import Text from '@components/Text';
 import {useMemoizedLazyIllustrations} from '@hooks/useLazyAsset';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
 import type {TranslationPaths} from '@src/languages/types';
-import type IconAsset from '@src/types/utils/IconAsset';
 
 const ILLUSTRATION_WIDTH = 100;
+const ILLUSTRATION_DEFAULT_HEIGHT = 100;
 
-type EmptyStateMessage = {
+type EmptyStateConfig = {
     titleKey: TranslationPaths;
     subtitleKey: TranslationPaths;
-    illustration: IconAsset;
+    illustrationName: IllustrationName;
     illustrationHeight: number;
 };
+
+const EMPTY_STATE_CONFIGS: EmptyStateConfig[] = [
+    {
+        titleKey: 'homePage.forYouSection.emptyStateMessages.allCaughtUpTitle',
+        subtitleKey: 'homePage.forYouSection.emptyStateMessages.keepAnEyeOut',
+        illustrationName: 'ThumbsUpStars',
+        illustrationHeight: ILLUSTRATION_DEFAULT_HEIGHT,
+    },
+    {
+        titleKey: 'homePage.forYouSection.emptyStateMessages.niceWork',
+        subtitleKey: 'homePage.forYouSection.emptyStateMessages.keepAnEyeOut',
+        illustrationName: 'SmallRocket',
+        illustrationHeight: ILLUSTRATION_DEFAULT_HEIGHT,
+    },
+    {
+        titleKey: 'homePage.forYouSection.emptyStateMessages.yeehaw',
+        subtitleKey: 'homePage.forYouSection.emptyStateMessages.allCaughtUpSubtitle',
+        illustrationName: 'CowboyHat',
+        illustrationHeight: ILLUSTRATION_DEFAULT_HEIGHT,
+    },
+    {
+        titleKey: 'homePage.forYouSection.emptyStateMessages.youWin',
+        subtitleKey: 'homePage.forYouSection.emptyStateMessages.allCaughtUpSubtitle',
+        illustrationName: 'Trophy1',
+        illustrationHeight: ILLUSTRATION_DEFAULT_HEIGHT,
+    },
+    {
+        titleKey: 'homePage.forYouSection.emptyStateMessages.allCaughtUpExclaim',
+        subtitleKey: 'homePage.forYouSection.emptyStateMessages.timeForABreak',
+        illustrationName: 'PalmTree',
+        illustrationHeight: ILLUSTRATION_DEFAULT_HEIGHT,
+    },
+    {
+        titleKey: 'homePage.forYouSection.emptyStateMessages.allCaughtUpExclaim',
+        subtitleKey: 'homePage.forYouSection.emptyStateMessages.timeForABreak',
+        illustrationName: 'FishbowlBlue',
+        illustrationHeight: ILLUSTRATION_DEFAULT_HEIGHT,
+    },
+    {
+        titleKey: 'homePage.forYouSection.emptyStateMessages.bullseye',
+        subtitleKey: 'homePage.forYouSection.emptyStateMessages.allCaughtUpSubtitle',
+        illustrationName: 'Target',
+        illustrationHeight: ILLUSTRATION_DEFAULT_HEIGHT,
+    },
+    {
+        titleKey: 'homePage.forYouSection.emptyStateMessages.allDone',
+        subtitleKey: 'homePage.forYouSection.emptyStateMessages.timeToRelax',
+        illustrationName: 'Chair',
+        illustrationHeight: ILLUSTRATION_DEFAULT_HEIGHT,
+    },
+    {
+        titleKey: 'homePage.forYouSection.emptyStateMessages.allClean',
+        subtitleKey: 'homePage.forYouSection.emptyStateMessages.timeToRelax',
+        illustrationName: 'Broom',
+        illustrationHeight: ILLUSTRATION_DEFAULT_HEIGHT,
+    },
+    {
+        titleKey: 'homePage.forYouSection.emptyStateMessages.takeItEasy',
+        subtitleKey: 'homePage.forYouSection.emptyStateMessages.everythingsTidiedUp',
+        illustrationName: 'House',
+        illustrationHeight: ILLUSTRATION_DEFAULT_HEIGHT,
+    },
+    {
+        titleKey: 'homePage.forYouSection.emptyStateMessages.nothingToShow',
+        subtitleKey: 'homePage.forYouSection.emptyStateMessages.caughtUpOnTasks',
+        illustrationName: 'ConciergeBot',
+        illustrationHeight: ILLUSTRATION_DEFAULT_HEIGHT,
+    },
+    {
+        titleKey: 'homePage.forYouSection.emptyStateMessages.tasksCompleted',
+        subtitleKey: 'homePage.forYouSection.emptyStateMessages.allCaughtUpSubtitle',
+        illustrationName: 'CheckboxText',
+        illustrationHeight: ILLUSTRATION_DEFAULT_HEIGHT,
+    },
+    {
+        titleKey: 'homePage.forYouSection.emptyStateMessages.youreNumber1',
+        subtitleKey: 'homePage.forYouSection.emptyStateMessages.caughtUpOnTasks',
+        illustrationName: 'Trophy',
+        illustrationHeight: ILLUSTRATION_DEFAULT_HEIGHT,
+    },
+    {
+        titleKey: 'homePage.forYouSection.emptyStateMessages.kaZap',
+        subtitleKey: 'homePage.forYouSection.emptyStateMessages.caughtUpOnTasks',
+        illustrationName: 'Flash',
+        illustrationHeight: ILLUSTRATION_DEFAULT_HEIGHT,
+    },
+    {
+        titleKey: 'homePage.forYouSection.emptyStateMessages.allDone',
+        subtitleKey: 'homePage.forYouSection.emptyStateMessages.caughtUpOnTasks',
+        illustrationName: 'F1Flags',
+        illustrationHeight: ILLUSTRATION_DEFAULT_HEIGHT,
+    },
+    {
+        titleKey: 'homePage.forYouSection.emptyStateMessages.finished',
+        subtitleKey: 'homePage.forYouSection.emptyStateMessages.caughtUpOnTasks',
+        illustrationName: 'Sunglasses',
+        illustrationHeight: ILLUSTRATION_DEFAULT_HEIGHT,
+    },
+    {
+        titleKey: 'homePage.forYouSection.emptyStateMessages.allCaughtUpSubtitle',
+        subtitleKey: 'homePage.forYouSection.emptyStateMessages.upcomingTodos',
+        illustrationName: 'Fireworks',
+        // Fireworks viewBox is 164x148, so height = 100 * (148/164) ≈ 90
+        illustrationHeight: 90,
+    },
+];
+
+const ILLUSTRATION_NAMES = EMPTY_STATE_CONFIGS.map((c) => c.illustrationName);
 
 function EmptyState() {
     const styles = useThemeStyles();
     const {translate} = useLocalize();
-    const illustrations = useMemoizedLazyIllustrations(['ThumbsUpStars', 'Fireworks']);
-
-    const defaultEmptyStateMessage: EmptyStateMessage = {
-        titleKey: 'homePage.forYouSection.emptyStateMessages.nicelyDone',
-        subtitleKey: 'homePage.forYouSection.emptyStateMessages.keepAnEyeOut',
-        illustration: illustrations.ThumbsUpStars,
-        // ThumbsUpStars viewBox is 68x68 (1:1 aspect ratio)
-        illustrationHeight: 100,
-    };
-
-    const emptyStateMessages: EmptyStateMessage[] = [
-        defaultEmptyStateMessage,
-        {
-            titleKey: 'homePage.forYouSection.emptyStateMessages.allCaughtUp',
-            subtitleKey: 'homePage.forYouSection.emptyStateMessages.upcomingTodos',
-            illustration: illustrations.Fireworks,
-            // Fireworks viewBox is 164x148, so height = 100 * (148/164) ≈ 90
-            illustrationHeight: 90,
-        },
-    ];
+    const illustrations = useMemoizedLazyIllustrations(ILLUSTRATION_NAMES);
 
     // Select a random empty state message on mount (will change on refresh/remount)
     // eslint-disable-next-line react-hooks/purity -- Random selection is intentional and should only happen once on mount
-    const randomIndex = Math.floor(Math.random() * emptyStateMessages.length);
-    const emptyStateMessage = emptyStateMessages.at(randomIndex) ?? defaultEmptyStateMessage;
+    const randomIndex = Math.floor(Math.random() * EMPTY_STATE_CONFIGS.length);
+    // eslint-disable-next-line rulesdir/prefer-at -- Using [0] for definite type since .at() returns T | undefined
+    const config = EMPTY_STATE_CONFIGS.at(randomIndex) ?? EMPTY_STATE_CONFIGS[0];
 
     return (
         <View style={styles.forYouEmptyStateContainer}>
             <ImageSVG
-                src={emptyStateMessage.illustration}
+                src={illustrations[config.illustrationName]}
                 width={ILLUSTRATION_WIDTH}
-                height={emptyStateMessage.illustrationHeight}
+                height={config.illustrationHeight}
             />
-            <Text style={styles.forYouEmptyStateTitle}>{translate(emptyStateMessage.titleKey)}</Text>
-            <Text style={styles.forYouEmptyStateSubtitle}>{translate(emptyStateMessage.subtitleKey)}</Text>
+            <Text style={styles.forYouEmptyStateTitle}>{translate(config.titleKey)}</Text>
+            <Text style={styles.forYouEmptyStateSubtitle}>{translate(config.subtitleKey)}</Text>
         </View>
     );
 }
