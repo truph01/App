d7c875f1691e63241ee82bb03c8aeca1b15cbefe
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2026-02-11 13:35:29 +0100

    address CR comments

--- src/pages/inbox/sidebar/FloatingActionButtonAndPopover.tsx
+++ src/pages/inbox/sidebar/FloatingActionButtonAndPopover.tsx
@@ -199,10 +199,14 @@ function FloatingActionButtonAndPopover({onHideCreateMenu, onShowCreateMenu, ref
     );
 
     const isReportInSearch = isOnSearchMoneyRequestReportPage();
-    const [groupPoliciesWithChatEnabled = CONST.EMPTY_ARRAY] = useOnyx(ONYXKEYS.COLLECTION.POLICY, {
-        selector: groupPaidPoliciesWithChatEnabled,
-        canBeMissing: true,
-    });
+    const [groupPoliciesWithChatEnabled = CONST.EMPTY_ARRAY] = useOnyx(
+        ONYXKEYS.COLLECTION.POLICY,
+        {
+            selector: groupPaidPoliciesWithChatEnabled,
+            canBeMissing: true,
+        },
+        [session?.email],
+    );
 
     /**
      * There are scenarios where users who have not yet had their group workspace-chats in NewDot (isPolicyExpenseChatEnabled). In those scenarios, things can get confusing if they try to submit/track expenses. To address this, we block them from Creating, Tracking, Submitting expenses from NewDot if they are:
