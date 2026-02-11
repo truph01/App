2b377a8a20c87181774eb797c7a6bf9b1c5105b2
Dariusz Biela <dariusz.biela.dev@gmail.com>
2026-02-11 16:20:29 +0100

    docs: add JSDoc descriptions to PASSKEY_TRANSPORT constants

--- src/CONST/index.ts
+++ src/CONST/index.ts
@@ -441,14 +441,22 @@ const CONST = {
 
     /**
      * WebAuthn AuthenticatorTransport values.
+     * Describes how the client communicates with a particular authenticator
+     * to perform a WebAuthn credential registration or authentication ceremony.
      * @see https://www.w3.org/TR/webauthn-3/#enum-transport
      */
     PASSKEY_TRANSPORT: {
+        /** Authenticator can be contacted over removable USB */
         USB: 'usb',
+        /** Authenticator can be contacted over Near Field Communication (NFC) */
         NFC: 'nfc',
+        /** Authenticator can be contacted over Bluetooth Low Energy (BLE) */
         BLE: 'ble',
+        /** Authenticator can be contacted over ISO/IEC 7816 smart card with contacts */
         SMART_CARD: 'smart-card',
+        /** Authenticator can be contacted using a combination of data-transport and proximity mechanisms (e.g. desktop auth via smartphone) */
         HYBRID: 'hybrid',
+        /** Authenticator is a platform authenticator contacted via client device-specific transport (not removable) */
         INTERNAL: 'internal',
     },
 
