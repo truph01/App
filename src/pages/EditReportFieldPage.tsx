812ed7ffa5e9bf986f08ec2c7fc34f61ef0eaf33
daledah <daleda261@outlook.com>
2026-02-11 23:21:50 +0700

    fix prettier

--- src/pages/EditReportFieldPage.tsx
+++ src/pages/EditReportFieldPage.tsx
@@ -121,8 +121,7 @@ function EditReportFieldPage({route}: EditReportFieldPageProps) {
     // Provide a default when the report name and the policy field list are empty
     /* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
     const fieldValue = isReportFieldTitle
-        ? getReportNameFromReportNameUtils(report, reportAttributesByReportID) ||
-          (isPolicyFieldListEmpty(policy) ? CONST.REPORT.DEFAULT_EXPENSE_REPORT_NAME : '')
+        ? getReportNameFromReportNameUtils(report, reportAttributesByReportID) || (isPolicyFieldListEmpty(policy) ? CONST.REPORT.DEFAULT_EXPENSE_REPORT_NAME : '')
         : (reportField.value ?? reportField.defaultValue);
     /* eslint-enable @typescript-eslint/prefer-nullish-coalescing */
 
