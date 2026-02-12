14be2f28af6b85c9117f599385dfad3a79151742
VickyStash <vikstash@gmail.com>
2026-02-12 17:28:11 +0100

    Update carousel container min height

--- src/components/ReportActionItem/MoneyRequestReportPreview/MoneyRequestReportPreviewContent.tsx
+++ src/components/ReportActionItem/MoneyRequestReportPreview/MoneyRequestReportPreviewContent.tsx
@@ -863,7 +863,7 @@ function MoneyRequestReportPreviewContent({
                                             <ActivityIndicator size={40} />
                                         </View>
                                     ) : (
-                                        <View style={[styles.flex1, styles.flexColumn, styles.overflowVisible, styles.minHeight55]}>
+                                        <View style={[styles.flex1, styles.flexColumn, styles.overflowVisible, styles.minHeight42]}>
                                             <FlashList
                                                 snapToAlignment="start"
                                                 decelerationRate="fast"
