bb39c1a5a3e24a82a8a9c8c10ea7207edbc6cbd9
Carlos Martins <cmartins@expensify.com>
2026-02-19 14:25:08 -0700

    update copy

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -6359,8 +6359,7 @@ const translations = {
                 matchTypeContains: 'Contains',
                 matchTypeExact: 'Exactly matches',
                 duplicateRuleTitle: 'Similar merchant rule already exists',
-                duplicateRulePrompt: (merchantName: string) =>
-                    `Do you want to save a new rule for "${merchantName}" even though you already have an existing one? Only the oldest matching rule will apply.`,
+                duplicateRulePrompt: (merchantName: string) => `Your existing rule for "${merchantName}" will take priority over this one. Save anyway?`,
                 saveAnyway: 'Save anyway',
                 applyToExistingUnsubmittedExpenses: 'Apply to existing unsubmitted expenses',
             },
