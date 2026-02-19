c3f9a1684b5b8b2839046f419950d80e1436fe9a
Rodrigo Lino da Costa <rodrigo@expensify.com>
2026-02-19 11:19:40 -0300

    make selector depend oonly on transaction

--- src/hooks/useReportTransactionsCollection.ts
+++ src/hooks/useReportTransactionsCollection.ts
@@ -1,12 +1,25 @@
+import {useCallback} from 'react';
 import type {OnyxCollection} from 'react-native-onyx';
-import {useAllReportsTransactionsAndViolations} from '@components/OnyxListItemProvider';
+import useOnyx from '@hooks/useOnyx';
+import ONYXKEYS from '@src/ONYXKEYS';
 import type {Transaction} from '@src/types/onyx';
+import type {ReportTransactionsAndViolationsDerivedValue} from '@src/types/onyx/DerivedValues';
 import {getEmptyObject} from '@src/types/utils/EmptyObject';
 
 function useReportTransactionsCollection(reportID?: string): OnyxCollection<Transaction> {
-    const allReportsTransactionsAndViolations = useAllReportsTransactionsAndViolations();
+    const transactionsSelector = useCallback(
+        (allReportsTransactionsAndViolations: ReportTransactionsAndViolationsDerivedValue | undefined) => {
+            return reportID ? allReportsTransactionsAndViolations?.[reportID]?.transactions : undefined;
+        },
+        [reportID],
+    );
 
-    return reportID ? (allReportsTransactionsAndViolations?.[reportID]?.transactions ?? getEmptyObject<OnyxCollection<Transaction>>()) : getEmptyObject<OnyxCollection<Transaction>>();
+    const [reportTransactions] = useOnyx(ONYXKEYS.DERIVED.REPORT_TRANSACTIONS_AND_VIOLATIONS, {
+        canBeMissing: true,
+        selector: transactionsSelector,
+    });
+
+    return reportTransactions ?? getEmptyObject<OnyxCollection<Transaction>>();
 }
 
 export default useReportTransactionsCollection;
