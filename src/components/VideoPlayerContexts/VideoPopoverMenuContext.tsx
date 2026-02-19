685f05eaa64875338f31d20ce816b0e68cab7521
Uzaifm127 <uzaifm127@gmail.com>
2026-02-19 21:12:55 +0530

    Merged the if conditions in updatePlaybackSpeed and left the comments regarding the changes

--- src/components/VideoPlayerContexts/VideoPopoverMenuContext.tsx
+++ src/components/VideoPlayerContexts/VideoPopoverMenuContext.tsx
@@ -26,10 +26,10 @@ function VideoPopoverMenuContextProvider({children}: ChildrenProps) {
     const updatePlaybackSpeed = useCallback(
         (speed: PlaybackSpeed) => {
             setCurrentPlaybackSpeed(speed);
-            if (!videoPopoverMenuPlayerRef.current) {
-                return;
-            }
-            if (videoPopoverMenuPlayerRef.current.playbackRate === speed) {
+
+            // We check if the player ref exists and if the playback rate is already set to the new speed to avoid redundant updates.
+            // On iOS, setting the playback rate can cause the video to resume playback if it was paused.
+            if (!videoPopoverMenuPlayerRef.current || videoPopoverMenuPlayerRef.current.playbackRate === speed) {
                 return;
             }
             videoPopoverMenuPlayerRef.current.playbackRate = speed;
