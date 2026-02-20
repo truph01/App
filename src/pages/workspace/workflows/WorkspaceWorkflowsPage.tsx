81182f60f8637cbdf010ea57ee686c14d43642ed
Francois Laithier <francois@expensify.com>
2026-02-19 18:13:01 -0800

    Show approval workflows on policies with DEW enabled

--- src/pages/workspace/workflows/WorkspaceWorkflowsPage.tsx
+++ src/pages/workspace/workflows/WorkspaceWorkflowsPage.tsx
@@ -175,7 +175,7 @@ function WorkspaceWorkflowsPage({policy, route}: WorkspaceWorkflowsPageProps) {
         Navigation.navigate(ROUTES.WORKSPACE_WORKFLOWS_APPROVALS_EXPENSES_FROM.getRoute(route.params.policyID));
     }, [policy, route.params.policyID, availableMembers, usedApproverEmails]);
 
-    const filteredApprovalWorkflows = policy?.approvalMode === CONST.POLICY.APPROVAL_MODE.ADVANCED ? approvalWorkflows : approvalWorkflows.filter((workflow) => workflow.isDefault);
+    const filteredApprovalWorkflows = [CONST.POLICY.APPROVAL_MODE.ADVANCED, CONST.POLICY.APPROVAL_MODE.DYNAMICEXTERNAL].includes(policy?.approvalMode) ? approvalWorkflows : approvalWorkflows.filter((workflow) => workflow.isDefault);
 
     const everyoneText = translate('workspace.common.everyone');
 
