a783652790c1f12f82f466a4f1dc113176961fd9
daledah <daleda261@outlook.com>
2026-02-21 11:57:21 +0700

    fix lint

--- src/pages/workspace/duplicate/WorkspaceDuplicateSelectFeaturesForm.tsx
+++ src/pages/workspace/duplicate/WorkspaceDuplicateSelectFeaturesForm.tsx
@@ -372,7 +372,6 @@ function WorkspaceDuplicateSelectFeaturesForm({policyID}: WorkspaceDuplicateForm
                             accessibilityState={{checked: isSelectAllChecked}}
                             sentryLabel={CONST.SENTRY_LABEL.WORKSPACE.DUPLICATE_SELECT_FEATURES_SELECT_ALL}
                             dataSet={{[CONST.SELECTION_SCRAPER_HIDDEN_ELEMENT]: true}}
-                            sentryLabel={CONST.SENTRY_LABEL.WORKSPACE_DUPLICATE_SELECT_FEATURES.SELECT_ALL}
                         >
                             <Text style={[styles.textLabelSupporting, styles.ph3]}>{translate('workspace.common.selectAll')}</Text>
                         </PressableWithFeedback>
