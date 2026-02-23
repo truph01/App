b49d41478ebfc409c2a7692250b8d5abda9c31dd
Jack Senyitko <jsenyitko@expensify.com>
2026-02-23 12:55:58 -0500

    use dep for selector

--- src/pages/inbox/report/ReportActionCompose/SuggestionMention.tsx
+++ src/pages/inbox/report/ReportActionCompose/SuggestionMention.tsx
@@ -93,7 +93,7 @@ function SuggestionMention({
     );
 
     const [conciergeReportID = ''] = useOnyx(ONYXKEYS.CONCIERGE_REPORT_ID);
-    const [mentionableReports] = useOnyx(ONYXKEYS.COLLECTION.REPORT, {selector: mentionableReportsSelector});
+    const [mentionableReports] = useOnyx(ONYXKEYS.COLLECTION.REPORT, {selector: mentionableReportsSelector}, [policyID]);
 
     const currentUserPersonalDetails = useCurrentUserPersonalDetails();
     const isMentionSuggestionsMenuVisible = !!suggestionValues.suggestedMentions.length && suggestionValues.shouldShowSuggestionMenu;
