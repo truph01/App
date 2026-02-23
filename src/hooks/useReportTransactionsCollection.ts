e7d2ca7e5b561d566d7866f0842d5a3a33ce3554
Rodrigo Lino da Costa <rodrigo@expensify.com>
2026-02-23 15:18:16 -0300

    fix ts error

--- src/hooks/useReportTransactionsCollection.ts
+++ src/hooks/useReportTransactionsCollection.ts
@@ -1,10 +1,10 @@
 import {useCallback} from 'react';
 import type {OnyxCollection} from 'react-native-onyx';
-import useOnyx from '@hooks/useOnyx';
 import ONYXKEYS from '@src/ONYXKEYS';
 import type {Transaction} from '@src/types/onyx';
 import type {ReportTransactionsAndViolationsDerivedValue} from '@src/types/onyx/DerivedValues';
 import {getEmptyObject} from '@src/types/utils/EmptyObject';
+import useOnyx from './useOnyx';
 
 function useReportTransactionsCollection(reportID?: string): OnyxCollection<Transaction> {
     const transactionsSelector = useCallback(
@@ -15,7 +15,6 @@ function useReportTransactionsCollection(reportID?: string): OnyxCollection<Tran
     );
 
     const [reportTransactions] = useOnyx(ONYXKEYS.DERIVED.REPORT_TRANSACTIONS_AND_VIOLATIONS, {
-        canBeMissing: true,
         selector: transactionsSelector,
     });
 
