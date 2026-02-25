f910b9e0304d6a6049ad35eeb0df03d9ba5906e0
allgandaf <whogandalf@gmail.com>
2026-02-25 09:48:57 +0530

    Allow Debit Card export in locations tag restriction check

--- src/pages/workspace/accounting/qbo/utils.ts
+++ src/pages/workspace/accounting/qbo/utils.ts
@@ -2,10 +2,11 @@ import CONST from '@src/CONST';
 import type {QBOConnectionConfig} from '@src/types/onyx/Policy';
 
 function shouldShowLocationsLineItemsRestriction(config?: QBOConnectionConfig): boolean {
-    return !(
-        config?.reimbursableExpensesExportDestination === CONST.QUICKBOOKS_REIMBURSABLE_ACCOUNT_TYPE.JOURNAL_ENTRY &&
-        config?.nonReimbursableExpensesExportDestination === CONST.QUICKBOOKS_NON_REIMBURSABLE_ACCOUNT_TYPE.CREDIT_CARD
-    );
+    const isJournalEntry = config?.reimbursableExpensesExportDestination === CONST.QUICKBOOKS_REIMBURSABLE_ACCOUNT_TYPE.JOURNAL_ENTRY;
+    const isCreditOrDebitCard =
+        config?.nonReimbursableExpensesExportDestination === CONST.QUICKBOOKS_NON_REIMBURSABLE_ACCOUNT_TYPE.CREDIT_CARD ||
+        config?.nonReimbursableExpensesExportDestination === CONST.QUICKBOOKS_NON_REIMBURSABLE_ACCOUNT_TYPE.DEBIT_CARD;
+    return !(isJournalEntry && isCreditOrDebitCard);
 }
 
 function shouldSwitchLocationsToReportFields(config?: QBOConnectionConfig): boolean {
