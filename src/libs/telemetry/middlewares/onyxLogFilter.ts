02afebe1d481b8f4f7a72f8e56bd555fd20dabac
Hubert Sosinski <hubert.sosinski@callstack.com>
2026-02-26 09:10:14 +0100

    remove check from if

--- src/libs/telemetry/middlewares/onyxLogFilter.ts
+++ src/libs/telemetry/middlewares/onyxLogFilter.ts
@@ -1,7 +1,7 @@
 import type {Log} from '@sentry/core';
 
 const onyxLogFilter = (log: Log): Log | null => {
-    if (log.level === 'error' && log.attributes?.['sentry.origin'] === 'auto.log.console' && typeof log.message === 'string' && log.message.startsWith('[Onyx]')) {
+    if (log.attributes?.['sentry.origin'] === 'auto.log.console' && typeof log.message === 'string' && log.message.startsWith('[Onyx]')) {
         return null;
     }
 
