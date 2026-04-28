bd3ad56d81abe8f44611fbf0e11a7578d63dab54
Lucien Akchoté <lucien@expensify.com>
2026-04-28 11:01:39 +0200

    update qbd flow hook

--- src/components/ConnectToQuickbooksDesktopFlow/index.tsx
+++ src/components/ConnectToQuickbooksDesktopFlow/index.tsx
@@ -1,12 +1,13 @@
 import {useEffect} from 'react';
-import useReusablePoliciesConnectedToQBD from '@hooks/useReusablePoliciesConnectedToQBD';
+import useReusablePoliciesConnectedTo from '@hooks/useReusablePoliciesConnectedTo';
 import Navigation from '@libs/Navigation/Navigation';
 import getQuickbooksDesktopSetupEntryRoute from '@pages/workspace/accounting/qbd/utils';
+import CONST from '@src/CONST';
 import ROUTES from '@src/ROUTES';
 import type {ConnectToQuickbooksDesktopFlowProps} from './types';
 
 function ConnectToQuickbooksDesktopFlow({policyID}: ConnectToQuickbooksDesktopFlowProps) {
-    const {hasReusablePoliciesConnectedToQBD} = useReusablePoliciesConnectedToQBD(policyID);
+    const {hasReusablePoliciesConnectedTo: hasReusablePoliciesConnectedToQBD} = useReusablePoliciesConnectedTo(CONST.POLICY.CONNECTIONS.NAME.QBD, policyID);
 
     useEffect(() => {
         if (hasReusablePoliciesConnectedToQBD) {
