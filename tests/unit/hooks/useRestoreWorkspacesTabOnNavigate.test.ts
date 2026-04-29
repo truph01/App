6f14a9bc88772bb1d348a543f773331ca797a280
Adam Grzybowski <adam.grzybowski@swmansion.com>
2026-04-29 18:32:17 +0200

    Add eslint-disable comment for __esModule naming convention in test mocks

--- tests/unit/hooks/useRestoreWorkspacesTabOnNavigate.test.ts
+++ tests/unit/hooks/useRestoreWorkspacesTabOnNavigate.test.ts
@@ -43,6 +43,7 @@ jest.mock('@libs/Navigation/Navigation', () => ({
 }));
 
 jest.mock('@libs/Navigation/helpers/getPathFromState', () => ({
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     __esModule: true,
     default: jest.fn(),
 }));
