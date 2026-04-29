cbe28fa635eda1bd39b1babf7176bcf1794cfd33
nkdengineer <nkdengineer@outlook.com>
2026-04-29 14:42:16 +0700

    fix ts check

--- src/libs/NextStepUtils.ts
+++ src/libs/NextStepUtils.ts
@@ -821,6 +821,7 @@ export {
     buildOptimisticNextStepForDynamicExternalWorkflowSubmitError,
     buildOptimisticNextStepForDynamicExternalWorkflowApproveError,
     buildOptimisticNextStepForDEWOffline,
+    buildOptimisticNextStepForPreventSelfApprovalsEnabled,
     // eslint-disable-next-line @typescript-eslint/no-deprecated
     buildNextStepNew,
 };
